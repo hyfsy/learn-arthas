622c399d5f64b97f88b316dc015acdc5e49597a5
hyfsy <1577975140@qq.com>
2021-10-15 19:29:04 +0800

    格式化

--- src/main/java/com/hyf/arthas/LearnArthasApplication.java
+++ src/main/java/com/hyf/arthas/LearnArthasApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnArthasApplication
-{
+public class LearnArthasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnArthasApplication.class, args);
